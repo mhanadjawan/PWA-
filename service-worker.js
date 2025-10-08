374ad3f626c876b274d874332580d88dc4ca6372
PWA <qwetyuioodn@gmail.com>
2025-10-08 22:05:53 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const CACHE_NAME = 'flashcards-cache-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './settings.html',
- './styles.css',
- './app.js',
-  './manifest.json',
-  './icons/android-launchericon-48-48.png',
-  './icons/android-launchericon-72-72.png',
-  './icons/android-launchericon-96-96.png',
-  '.icons/android-launchericon-144-144.png',
-  './icons/android-launchericon-192-192.png',
-  './icons/android-launchericon-512-512.png',
-  './icons/100.png',
-  './icons/128.png',
-  './icons/256.png',
-  './icons/512.png',
-  './icons/192.png',
-  './icons/1024.png'
+  '/',
+  '/index.html',
+  '/settings.html',
+ '/styles.css',
+ '/app.js',
+  '/manifest.json',
+  '/icons/android-launchericon-48-48.png',
+  '/icons/android-launchericon-72-72.png',
+  '/icons/android-launchericon-96-96.png',
+  '/icons/android-launchericon-144-144.png',
+  '/icons/android-launchericon-192-192.png',
+  '/icons/android-launchericon-512-512.png',
+  '/icons/100.png',
+  '/icons/128.png',
+  '/icons/256.png',
+  '/icons/512.png',
+  '/icons/192.png',
+  '/icons/1024.png'
  
 
 ];
@@ -50,3 +50,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
